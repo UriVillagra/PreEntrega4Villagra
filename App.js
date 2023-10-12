8c54659ef721f8b1ec47147b2f8ef489c5d3c991
Urias Villagra <emavillagra00@gmail.com>
2023-10-11 23:19:54 -0300

    PreEntrega6Villagra

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens';
 import { Products } from './src/screens';
+import { useState } from 'react';
 
 export default function App() {
+  const [categorySelected, setCategorySelected] = useState('') 
+
+
   return (
         <Products category={"mens-watches"} />
   )
